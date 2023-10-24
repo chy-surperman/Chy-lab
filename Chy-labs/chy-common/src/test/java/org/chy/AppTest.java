c1e31651d2afdd49e45556311a665bae103ad83e
陈海荣 <l>
2023-10-24 19:28:00 +0800

    Initial commit

---
+++ Chy-labs/chy-common/src/test/java/org/chy/AppTest.java
@@ -0,0 +1,20 @@
+package org.chy;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
